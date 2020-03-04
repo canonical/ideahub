47d88b51199d9eb2fa9226dddc09dff588964f8b
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-03-04 09:54:40 +0100

    Refactor layout

--- src/views/layout/Layout.js
+++ src/views/layout/Layout.js
@@ -3,60 +3,27 @@
 
 import React from "react";
 
-import logIn from "../../actions/logIn";
-import FirebaseAuth from "../misc/FirebaseAuth";
+import Header from "../../components/Header/Header";
+
+import "./_layout.scss";
 
 import { Strip, Row, Col } from "@canonical/react-components";
 
 const Layout = ({ children }) => (
-  <div>
-    <Strip shallow element="section" type="light">
-      <Row>
-        <Col size="6">
-          <a href="/">Ideahub</a>
-        </Col>
-
-        <Col size="6" className="u-align--right">
-          <a href="/search">
-            <span role="img" aria-label="search">
-              🔎
-            </span>
-          </a>{" "}
-          <FirebaseAuth>
-            {({ isLoading, error, auth }) => {
-              if (isLoading) {
-                return "...";
-              }
-              if (error) {
-                return "⚠️ login error";
-              }
-              if (auth) {
-                return (
-                  <a href={`/account`}>
-                    <span role="img" aria-label="account">
-                      👤
-                    </span>
-                  </a>
-                );
-              } else {
-                return <button onClick={logIn}>log in</button>;
-              }
-            }}
-          </FirebaseAuth>
-        </Col>
-      </Row>
-    </Strip>
-
-    <Strip element="section">
-      <Row>{children}</Row>
-    </Strip>
-
-    <Strip shallow element="section" type="dark">
-      <Row>
-        <div>© {new Date().getFullYear()}</div>
-      </Row>
-    </Strip>
-  </div>
+  <>
+    <div className="row">
+      <Header />
+    </div>
+    {children}
+    <footer>
+      <div className="row">
+        <p>
+          &copy; {new Date().getFullYear()} Canonical Ltd. Ubuntu and Canonical
+          are registered trademarks of Canonical Ltd.
+        </p>
+      </div>
+    </footer>
+  </>
 );
 
 export default Layout;
