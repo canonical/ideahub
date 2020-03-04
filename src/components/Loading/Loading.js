a925f650b8e9bd03672e8c04f45c3b91af2df8f8
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-03-04 16:10:45 +0100

    Use className instead of class

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -4,8 +4,8 @@ import "./_loading.scss";
 
 export default function Loading() {
   return (
-    <div class="loading">
-      <i class="p-icon--spinner u-animation--spin is-light"></i>
+    <div className="loading">
+      <i className="p-icon--spinner u-animation--spin is-light"></i>
     </div>
   );
 }
